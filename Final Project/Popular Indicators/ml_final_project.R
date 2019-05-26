531d1d4d3e7863dfddb850aa1d855a7bed365e2a
xiahao <wangxiahaosin@gmail.com>
2019-05-25 22:16:17 -0700

    adds comments

--- Final Project/Popular Indicators/ml_final_project.R
+++ Final Project/Popular Indicators/ml_final_project.R
@@ -2,15 +2,13 @@ suppressMessages(require(data.table))
 
 rm(list=ls())
 
+# clean up and reshape the data
 world_bank_raw_data <-as.data.table(read.csv("world_bank_data.csv", header = T, sep = ","))
 world_bank_raw_data <- world_bank_raw_data[ 1:5425,]
-
-
 world_bank_raw_data[, Series.Code := NULL]
 world_bank_raw_data[, Country.Name := NULL]
 
 colnames(world_bank_raw_data) <- c("Factor_name", "Country_code", 1960:2018)
-
 reshaped_wb_data <- melt(world_bank_raw_data, id.vars = c("Country_code", "Factor_name"), 
                          variable.name = "year", 
                          value.name = "Factor_value")
